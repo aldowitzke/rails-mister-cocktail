2252701b2f09b579eea51d5734bcaf6083eae77d
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-14 20:04:55 -0200

    backend done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  # get 'doses/index'
+  # get 'doses/show'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/edit'
+  # get 'doses/update'
+  # get 'doses/destroy'
+
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # get 'cocktails/edit'
+  # get 'cocktails/update'
+  # get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cocktails do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: %i[destroy]
 end
