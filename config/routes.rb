5f8838ef4769587f2243d06c15276fa9605fede7
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-17 15:39:59 -0300

    scss show page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # get 'cocktails/new'
   # get 'cocktails/create'
   # get 'cocktails/edit'
-  # get 'cocktails/update'
+  # post 'cocktails/update'
   # get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
